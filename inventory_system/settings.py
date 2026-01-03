900cbb09345ecb1cc3bdeecd3e1957cd1d2e018f
sarbas1212 <sarbaskallinga@gmail.com>
2026-01-03 15:51:31 +0530

    Use .env for database config and ignore environment files

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from decouple import config
 from datetime import timedelta
 from pathlib import Path
 
@@ -97,10 +98,15 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
